0ab4011e706d2d15ffc78b0c4891e5169733aedf
antonsetiazi <antons899@gmail.com>
2026-05-02 16:00:16 +0700

    remove ustadzku from main repo

--- verticals/ustadzku/ui/bootstrap.py
+++
@@ -1,8 +0,0 @@
-# verticals/ustadzku/ui/bootstrap.py
-
-from verticals.ustadzku.ui.pages.home import guest_home 
-from verticals.ustadzku.ui.pages.dashboard import admin_dashboard 
-from verticals.ustadzku.ui.pages.dashboard import user_dashboard
-from verticals.ustadzku.ui.pages.dashboard import partner_dashboard
-
-# from ..ui import menus
